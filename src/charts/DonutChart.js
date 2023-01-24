c5b14fb11c9ebb91f1852cd1a6c39cd265fd3754
Jalal Fathi <jfathi.mail@gmail.com>
2023-01-24 20:35:02 +0100

    chore: use d3 cdn with simple donut chart

--- src/charts/DonutChart.js
+++ src/charts/DonutChart.js
@@ -1,27 +1,28 @@
 import { html, css, LitElement } from 'lit';
 
-// import { select } from 'd3-selection';
-// import { pie, arc } from 'd3-shape';
-// import { scaleOrdinal } from 'd3-scale';
-// import { quantize } from 'd3-interpolate';
-// import { interpolateCool } from 'd3-scale-chromatic';
+const d3 = window.d3;
 
-export class DonutChart extends LitElement {
-  static styles = css`p { color: blue }`;
+class DonutChart extends LitElement {
+  static styles = css`
+    :host {
+      display: inline-block;
+      box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    }
+  `;
 
   static properties = {
     data: { type: Array },
   };
 
   get svg() {
-    return select(this.shadowRoot.querySelector('.chart'))
-      .attr('width', this.width)
-      .attr('height', this.height);
+    return d3.select(this.shadowRoot.querySelector('.chart'))
   }
 
   constructor() {
     super();
-    this.data = [];
+
+    this.data = [2, 4, 8, 10];
+    this.colors = ["#98abc5", "#8a89a6", "#7b6888", "#6b486b"];
   }
 
   updated(changedProps) {
@@ -31,69 +32,41 @@ export class DonutChart extends LitElement {
   }
 
   _buildChart(svg) {
-    // let radius = Math.min(this.width, this.height) / 2;
-    // let arcShape = arc()
-    //   .innerRadius(radius * 0.4)
-    //   .outerRadius(radius - 1);
-    // let arcShapeLabels = arc()
-    //   .outerRadius(radius - 1)
-    //   .innerRadius(radius * 0.7);
+    const width = svg.attr("width");
+    const height = svg.attr("height");
+    const radius = Math.min(width, height) / 2;
+    const g = svg.append("g").attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
-    // let colorScale = scaleOrdinal()
-    //   .domain(this.data.map((d) => d.tag))
-    //   .range(
-    //     quantize(
-    //       (t) => interpolateCool(t * 0.8 + 0.1),
-    //       this.data.length
-    //     ).reverse()
-    //   );
+    const color = d3.scaleOrdinal(this.colors);
 
-    // let pieDataStructure = pie()
-    //   .sort(null)
-    //   .value((d) => d.value)(this.data);
+    // Generate the pie
+    const pie = d3.pie();
 
-    // svg
-    //   .append('g')
-    //   .attr('transform', `translate(${this.width / 2}, ${this.height / 2})`)
-    //   .attr('stroke', 'pink')
-    //   .selectAll('path')
-    //   .data(pieDataStructure)
-    //   .join('path')
-    //   .attr('fill', (d) => colorScale(d.data.tag))
-    //   .attr('d', arcShape)
-    //   .on('mouseenter', function () {
-    //     select(this).attr(
-    //       'class',
-    //       select(this).classed('selected') ? null : 'selected'
-    //     );
-    //   })
-    //   .on('mouseleave', function () {
-    //     select(this).attr(
-    //       'class',
-    //       select(this).classed('selected') ? null : 'selected'
-    //     );
-    //   });
+    // Generate the arcs
+    const arc = d3.arc()
+                .innerRadius(0)
+                .outerRadius(radius);
 
-    // svg
-    //   .append('g')
-    //   .attr('transform', `translate(${this.width / 2}, ${this.height / 2})`)
-    //   .attr('font-family', 'sans-serif')
-    //   .attr('font-size', 14)
-    //   .attr('font-weight', 800)
-    //   .attr('text-anchor', 'middle')
-    //   .selectAll('text')
-    //   .data(pieDataStructure)
-    //   .join('text')
-    //   .attr(
-    //     'transform',
-    //     (d) =>
-    //       `translate(${arcShapeLabels.centroid(d)[0] * 0.8},${arcShapeLabels.centroid(d)[1] * 0.8
-    //       })`
-    //   )
-    //   .call((text) => text.append('tspan').text((d) => d.data.tag));
+    //Generate groups
+    const arcs = g.selectAll("arc")
+                .data(pie(this.data))
+                .enter()
+                .append("g")
+                .attr("class", "arc")
+
+    //Draw arc paths
+    arcs.append("path")
+        .attr("fill", function(d, i) {
+            return color(i);
+        })
+        .attr("d", arc);
   }
 
   render() {
-    return html`Hello <svg class="chart"></svg>`;
+    return html`<svg width="300" height="200" class="chart"> </svg>`;
   }
+}
+
+export {
+  DonutChart,
 }
